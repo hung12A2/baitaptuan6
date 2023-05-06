c107528e77d5876ff1cbf7985b0489e45965e078
Hưng Nguyễn <hung131119782002@gmail.com>
2023-05-06 23:57:35 +0700

    baitaptuan6

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const authRouter = require('./routes/auth')
+const postRouter = require('./routes/post')
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(
+			`mongodb+srv://hung131119782002:12345678aA@clustertodo1.0k0mr2u.mongodb.net/?retryWrites=true&w=majority`,
+			{
+				useCreateIndex: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false
+			}
+		)
+
+		console.log('MongoDB connected')
+	} catch (error) {
+		console.log(error.message)
+		process.exit(1)
+	}
+}
+
+connectDB()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postRouter)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
